e6be90c90738f7a44d0571dd1cf2ae96d6c76e11
hunterborwick <62222564+hunterborwick@users.noreply.github.com>
2020-03-27 11:01:37 -0400

    Initial commit

---
+++ first.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title('Test')
+st.write('User Input')
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
